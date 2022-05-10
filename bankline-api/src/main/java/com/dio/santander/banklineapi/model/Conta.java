42f796bdce00af3f2531efc98951cddbb3bb7610
Renata R C Pedreschi <99511265+RenataRCPedreschi@users.noreply.github.com>
2022-05-10 17:43:26 -0300

    API santander dio

---
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -0,0 +1,29 @@
+package com.dio.santander.banklineapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Conta {
+	@Column(name = "conta_numero")
+	private Long numero;
+	@Column(name = "conta_saldo")
+	private double saldo;
+	
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
---
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/model/MovimentacaoTipo.java
@@ -0,0 +1,9 @@
+package com.dio.santander.banklineapi.model;
+
+public enum MovimentacaoTipo {
+	
+	
+	RECEITA,
+	DESPESA
+
+}
